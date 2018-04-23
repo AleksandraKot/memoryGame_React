5a918fc9373f2550961ad4a5777445a301e9f102
Aleksandra <pyzakot@gmail.com>
2018-04-23 18:11:02 +0200

    Working environment set up

---
+++ src/jsx/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', function() {
+        
+    class App extends React.Component {
+    render() {
+        return (
+        <div>
+            ASD
+        </div>
+        )
+    }
+    }
+
+    ReactDOM.render(
+        <div><App/></div>, document.getElementById('app'));
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,45 @@
-//Konfiguracja Webpack
-const path = require("path");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-  entry: "./js/memory.jsx",
-  output: { path: path.resolve("dist"), filename: "out.js" },
-  watch: true,
-  module: {
-    loaders: [
-      {
-        test: /\.jsx$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        query: { presets: ["es2015", 'stage-2', "react"] }
-      }
+    entry: ["whatwg-fetch","./src/jsx/App.jsx","./src/scss/style.scss"],
+    output: { filename: "./dist/js/out.js" },
+    devServer: {
+        inline: true,
+        contentBase: './',
+        port: 3000
+    },
+    watch: true,
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: { presets: ['es2015', 'stage-2', 'react'] }
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('./dist/css/style.css')
     ]
-  },
-  devtool: 'source-map'
 };
